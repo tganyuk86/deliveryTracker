00dc9cb4d83fa17c200b2e214a16ae5ef6609bfe
tganyuk86 <tganyuk86@gmail.com>
2020-05-06 21:16:23 -0400

    Update loadreport.blade.php

--- resources/views/loadreport.blade.php
+++ resources/views/loadreport.blade.php
@@ -24,27 +24,18 @@
                     @foreach($orders as $order)
                       <tr id="{{$order->id}}">
                         <td>{{$order->customer()->address}}<br>
-                          <sup>{{ $order->created_at->diffForHumans() }}<sup>
-                        </td>
-                        <td>
-                          {{$order->customer()->name}}
+                          
                         </td>
                         <td>
                           {{$order->order}}
                         </td>
                         <td>
-                          {{$order->value}}
+                          ${{$order->value}}
                         </td>
-                        <td>
-                          <button type="button" class="btn btn-primary assignDriver" data-orderid="{{$order->id}}" data-toggle="modal" data-target="#assignDriver">
-                            {{ $order->driver()->isAdmin() ? 'Unassigned' : $order->driver()->name}}
-                          </button>
-
-                          <button type="button" class="btn btn-primary up" data-direction="up">Up</button>
-                          <button type="button" class="btn btn-primary down" data-direction="down">Down</button>
-
-                          <!-- <input type="hidden" name="orders[]" value="{{$order->id}}"> -->
-                        </td>
+						<td>
+							<sup>{{ $order->created_at->diffForHumans() }}<sup>
+						</td>
+                       
                       </tr>
                     @endforeach
 					
