18bd7795f2317463ded962c60460262dfccba5a4
tganyuk86 <tganyuk86@gmail.com>
2020-05-08 18:54:48 -0400

    Update customers.blade.php

--- resources/views/customers.blade.php
+++ resources/views/customers.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<div class="container">
+<div class="container-fluid">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -37,7 +37,7 @@
                         {{$customer->name}}
                       </div>
                       <div class="col-md-2">
-<a href="/neworder/{{$customer->id}}"><button>Place Order</button></a>
+<a href="/neworder/{{$customer->id}}"><button>Go</button></a>
                       </div>
                     </div>
 
