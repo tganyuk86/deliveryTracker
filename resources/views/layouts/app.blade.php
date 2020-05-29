f25cca86628d19fb8d536549cf06e01e64695b35
tganyuk86 <tganyuk86@gmail.com>
2020-05-29 11:52:24 -0400

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -112,6 +112,12 @@
                             </li>
                             
                         @else
+							<li class="nav-item">
+                                <a class="nav-link" href="{{ route('neworder') }}">New Order</a>
+                            </li>
+							<li class="nav-item">
+                                <a class="nav-link" href="/admin">Admin</a>
+                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
