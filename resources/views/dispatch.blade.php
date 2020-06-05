6b0732ee0ee867b4b042aaa3a65cb4c7ff1a8e1e
tganyuk86 <tganyuk86@gmail.com>
2020-06-04 20:22:55 -0400

    gg

--- resources/views/dispatch.blade.php
+++ resources/views/dispatch.blade.php
@@ -49,7 +49,7 @@
                           Cancel
                         </button> -->
 						<a href="{{ route('mark.done', ['id' => $Order->id]) }}" class="btn btn-outline-success">Done</a>
-						<a href="{{ route('mark.x', ['id' => $Order->id]) }}" class="btn btn-outline-error">Cancel</a>
+						<a href="{{ route('cancelOrder', ['id' => $Order->id]) }}" class="btn btn-outline-error">Cancel</a>
 						<a href="/admin/orders/{{$Order->id}}/edit" class="btn btn-outline-warning">Edit</a>
 
                       </div>
