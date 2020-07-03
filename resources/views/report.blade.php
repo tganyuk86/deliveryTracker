e5498fefe186c54a034979c71e838471ca4f3a4f
tganyuk86 <tganyuk86@gmail.com>
2020-07-03 16:38:51 -0400

    Update report.blade.php

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -6,6 +6,26 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
+                <div class="card-header">Todays Report</div>
+
+                <div class="card-body">
+
+                    <form action="{{ route('loadReport') }}" method="post">
+
+						<input type="hidden" name="repDate" value="{{ Carbon::now() }}" />
+                   
+                    @csrf
+
+                   
+                    <input type="submit" class="form-control btn" name="" value="Load">
+                    </form>
+
+
+                      
+                </div>
+            </div>
+			
+			<div class="card">
                 <div class="card-header">Select Date</div>
 
                 <div class="card-body">
