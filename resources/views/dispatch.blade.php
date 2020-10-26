39ce81d86f51d914e7e3347cb0e43c9b4481066e
tganyuk86 <tganyuk86@gmail.com>
2020-10-26 14:13:51 -0400

    Update dispatch.blade.php

--- resources/views/dispatch.blade.php
+++ resources/views/dispatch.blade.php
@@ -106,14 +106,44 @@
                       </div>
                       <div class="col-md-4">
                         
-                      
-						<a href="{{ route('mark.done', ['id' => $Order->id]) }}" class="btn btn-success">Done</a>
+                      	<a href="#" class="btn btn-outline-success doneButton">Done</a>
+
+						<!-- <a href="{{ route('mark.done', ['id' => $Order->id]) }}" class="btn btn-success">Done</a> -->
 						<a href="/admin/orders/{{$Order->id}}/edit" class="btn btn-warning">Edit</a>
 						<a href="{{ route('cancelOrder', ['id' => $Order->id]) }}" class="btn btn-danger">Cancel</a>
 
                       </div>
 
                     </div>
+					
+					<form action="{{ route('updateOrder') }}" method="post">
+						<div class="row doneInfo">
+							@csrf
+							<input type="hidden" name="orderID" value="{{ $Order->id }}" />
+							<div class="col-md-4">
+							<select name="payType" class="form-control">
+								<option value="cash" {{ $Order->payType == 'cash' ? 'selected' : '' }} >Cash</option>
+								<option value="emt" {{ $Order->payType == 'emt' ? 'selected' : '' }} >E-Transfer</option>
+							</select>
+							</div>
+							<div class="col-md-4">
+							<select name="status" class="form-control">
+								<option value="waiting" {{ $Order->status == 'waiting' ? 'selected' : '' }} >Waiting</option>
+								<option value="pending" {{ $Order->status == 'pending' ? 'selected' : '' }} >Pending Payment</option>
+								<option value="done" {{ $Order->status == 'done' ? 'selected' : '' }} >Done</option>
+							</select>
+							</div>
+							<div class="col-md-4">
+							<input type="number" name="outstanding" value="{{ $Order->outstanding }}" class="form-control" />
+							</div>
+							<div class="col-md-4">
+							<textarea name="customerNotes" placeholder="Customer Notes" class="form-control" >{{ $Order->customer()->notes }}</textarea>
+							</div>
+							<button class="btn btn-success" >Finish</button>
+							
+						</div>
+					</form> 
+					
 					<div class="row done" >
 						<div class="col-md-12">{{$Order->notes}}</div>
 					</div>
@@ -146,6 +176,10 @@
                       </div>
 
                     </div>
+					
+					
+					
+						
 					<div class="row done" >
 						<div class="col-md-12">{{$Order->notes}}</div>
 					</div>
