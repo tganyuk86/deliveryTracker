f5839a0206f2dd097a1f18b1fc905a4ffff50851
tganyuk86 <tganyuk86@gmail.com>
2020-12-13 23:15:11 -0800

    Update sendSMS.blade.php

--- resources/views/sendSMS.blade.php
+++ resources/views/sendSMS.blade.php
@@ -50,7 +50,7 @@
                         {{count($customer->orders())}}
                       </div>
                       <div class="col-md-4">
-                        {{$customer->orders()->last()->created_at}}
+                        {{count($customer->orders()) ? $customer->orders()->last()->created_at : '-'}}
                       </div>
                     </div>
                   @endforeach
