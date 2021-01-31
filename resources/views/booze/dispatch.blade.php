412073b459820c022e225b98fb69853155f3a7c4
Thomas <tganyuk86@gmail.com>
2021-01-30 22:24:46 -0600

    ss

---
+++ resources/views/booze/dispatch.blade.php
@@ -0,0 +1,266 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container-fluid">
+    @if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
+        </div>
+    @endif
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">Orders
+                <input data-filter="orders" placeholder="Filter" class="myInput" />
+                </div>
+
+                <div class="card-body">
+                 
+
+                  @foreach($Orders as $key => $Order)
+                    <div class="row" data-filtered="orders">
+                     <!--  <div class="col-md-2">
+                        {{ $Order->called_at}}
+
+                      </div> -->
+                      <div class="col-md-4">
+                        <b>#{{$key++}}:</b><br>
+                        {{$Order->customer()->address}}<br>
+                        <sup>{{$Order->customer()->name}}</sup>
+                        <sup>{{$Order->customer()->phone}}</sup>
+                        <sup>{{ $Order->created_at->diffForHumans() }}<sup>
+                      </div>
+                      <div class="col-md-4">
+                        <ul>
+                          {!! $Order->order !!}
+                          <li>Total: ${{$Order->value}}({{$Order->payType}})</li>
+                        </ul>
+                      </div>
+                      <div class="col-md-4">
+                        
+                        <button type="button" class="btn btn-primary assignDriver" data-orderid="{{$Order->id}}" data-toggle="modal" data-target="#assignDriver">
+                          {{ $Order->driver()->isAdmin() ? 'House' : $Order->driver()->name}}
+                        </button>
+                        <!-- <a href="{{ route('editOrder', $Order->id) }}">
+                          <button type="button" class="btn btn-warning editOrder" data-orderid="{{$Order->id}}">
+                            Edit
+                          </button>
+                        </a>
+
+                        <button type="button" class="btn btn-danger cancelOrder" data-orderid="{{$Order->id}}">
+                          Cancel
+                        </button> -->
+						<a href="{{ route('mark.done', ['id' => $Order->id]) }}" class="btn btn-success">Done</a>
+						<a href="/admin/orders/{{$Order->id}}/edit" class="btn btn-warning">Edit</a>
+						<a href="{{ route('cancelOrder', ['id' => $Order->id]) }}" class="btn btn-danger">Cancel</a>
+
+                      </div>
+
+                    </div>
+				<hr />
+                  @endforeach
+
+
+                  @foreach($pendingOrders as $Order)
+                    <div class="row pending" data-filtered="orders">
+                      
+                      <div class="col-md-4">
+                        {{$Order->customer()->address}}
+                        <sup>{{$Order->customer()->name}}</sup>
+                        <sup>{{$Order->customer()->phone}}</sup>
+                        <sup>{{ $Order->created_at->diffForHumans() }}<sup>
+                      </div>
+                      <div class="col-md-4">
+                        <ul>
+                          {!! $Order->order !!}
+                          <li>Total: ${{$Order->value}}({{$Order->payType}})</li>
+                        </ul>
+                      </div>
+                      <div class="col-md-4">
+                        
+                      
+						<a href="{{ route('mark.done', ['id' => $Order->id]) }}" class="btn btn-success">Done</a>
+						<a href="/admin/orders/{{$Order->id}}/edit" class="btn btn-warning">Edit</a>
+						<a href="{{ route('cancelOrder', ['id' => $Order->id]) }}" class="btn btn-danger">Cancel</a>
+
+                      </div>
+
+                    </div>
+					<div class="row done" >
+						<div class="col-md-12">{{$Order->notes}}</div>
+					</div>
+					<div class="row done" >
+						<div class="col-md-12">{{$Order->customer()->notes}}</div>
+					</div>
+
+                  @endforeach
+                  @foreach($doneOrders as $Order)
+                    <div class="row done" data-filtered="orders">
+                      
+                      <div class="col-md-4">
+                        {{$Order->customer()->address}}
+                        <sup>{{$Order->customer()->name}}</sup>
+                        <sup>{{$Order->customer()->phone}}</sup>
+                        <sup>{{ $Order->created_at->diffForHumans() }}<sup>
+                      </div>
+                      <div class="col-md-4">
+                        <ul>
+                          {!! $Order->order !!}
+                          <li>Total: ${{$Order->value}}({{$Order->payType}})</li>
+                        </ul>
+                      </div>
+                      <div class="col-md-4">
+                        
+                      
+						<a href="/admin/orders/{{$Order->id}}/edit" class="btn btn-warning">Edit</a>
+						<a href="{{ route('cancelOrder', ['id' => $Order->id]) }}" class="btn btn-danger">Cancel</a>
+
+                      </div>
+
+                    </div>
+					<div class="row done" >
+						<div class="col-md-12">{{$Order->notes}}</div>
+					</div>
+					<div class="row done" >
+						<div class="col-md-12">{{$Order->customer()->notes}}</div>
+					</div>
+
+                  @endforeach
+
+
+                      <script type="text/javascript">
+
+
+                    
+
+                      </script>
+                </div>
+            </div>
+            <div class="card" style="display:none">
+                <div class="card-header">Stock</div>
+
+                <div class="card-body">
+
+
+
+                @foreach($stocks as $product => $data)
+                    <h2>{{$product}}</h2>
+                     <div class="row ">
+                        @foreach($data as $type => $stock)
+                          <div class="col-md-3">
+                            {{$type}} (${{$stock->price}})
+                          </div>
+                        @endforeach
+                      </div>
+                @endforeach
+                        
+                       
+
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">Map</div>
+
+                <div class="card-body">
+
+
+                      <div class="myMap" id="mymap"></div>
+
+
+
+
+                      <script type="text/javascript">
+
+
+                        var Orders = <?php print_r(json_encode($Orders)) ?>;
+
+
+                        var mymap = new GMaps({
+                          el: '#mymap',
+                          lat: 43.6532,
+                          lng: -79.3832,
+                          zoom:11
+                        });
+
+						ind = -1;
+                        $.each( Orders, function( index, value ){
+							ind++;
+                            mymap.addMarker({
+                              lat: value.lat,
+                              lng: value.lon,
+                              title: value.customerData.address,
+                              label: '('+ind+')',
+                              click: function(e) {
+                                alert('This is '+value.lat+'.');
+                              }
+                            }); 
+
+                       });
+
+                        $('.assignDriver').on('click', function () {
+                          $('[name="orderID"]').val($(this).data('orderid'));
+                        });
+						
+						setTimeout(function() {
+						  location.reload();
+						}, 180000);
+						
+						@foreach(Auth::user()->drivers() as $driver)
+						@if($driver->lat)
+						 mymap.addMarker({
+                              lat: {{ $driver->lat }},
+                              lng: {{ $driver->lon }},
+                              title: '{{ $driver->name }}',
+                              label: '{{ substr($driver->name, 0, 1) }}',
+                              click: function(e) {
+                                alert('{{ $driver->updated_at->diffForHumans()}}');
+                              }
+                            });
+						@endif
+                        @endforeach
+                      </script>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            
+        </div>
+
+    </div>
+</div>
+
+
+<!-- Modal -->
+<div class="modal fade" id="assignDriver" tabindex="-1" role="dialog" aria-labelledby="assignDriverLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+        <form action="/assign" method="POST">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Assign to</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+          @csrf
+          <select name="driverID">
+            @foreach($drivers as $driver)
+              <option value="{{$driver->id}}">{{$driver->name}}</option>
+            @endforeach
+          </select>
+          <input type="hidden" name="orderID">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Save changes</button>
+      </div>
+        </form>
+    </div>
+  </div>
+</div>
+@endsection
---
+++ resources/views/booze/neworder.blade.php
@@ -0,0 +1,307 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">Info</div>
+
+                <div class="card-body">
+
+                    <form action="{{ route('saveorder') }}" method="post">
+                        
+                    <input type="text" class="form-control" name="address" placeholder="Address" value="{{$customer->address}}" id="myInput">
+                    
+                     <!-- <label>Phone</label> -->
+                    <input type="text" class="form-control" name="phone" value="{{$customer->phone}}" placeholder="Phone"  required>
+                    
+                     <!-- <label>Name</label> -->
+                    <input type="text" class="form-control" name="name" value="{{$customer->name}}" placeholder="Name"  required>
+                    <input type="button" class="form-control btn btn-info" id="getorder" value="Fill">
+                    
+                     <label>Delivery fee</label>
+                     <select class="form-control" name="deliveryFee">
+                       <option value="0">No</option>
+                       <option value="auto">Auto</option>
+                       <option value="1">Yes</option>
+                     </select>
+           
+           
+                     <label>Payment Type</label>
+                     <select class="form-control" name="payType" required>
+           <option value="" >Choose...</option>
+                       <option value="cash">Cash</option>
+                       <option value="emt">E-Transfer</option>
+                     </select>
+                    
+                    <label>Order</label>
+                    @foreach($products as $cat => $prods)
+                    {{$cat}}
+                    @foreach($prods as $prod => $row)
+
+                    <div class="accordion" id="accordionExample">
+            <div class="card">
+              <div class="card-header" id="headingOne">
+                <h2 class="mb-0">
+                  <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapse{{$loop->iteration}}{{$loop->parent->iteration}}" aria-expanded="true" aria-controls="collapseOne">
+                    {{$prod}}
+                  </button>
+                </h2>
+              </div>
+
+              <div id="collapse{{$loop->iteration}}{{$loop->parent->iteration}}" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                <div class="card-body">
+            @foreach($row as $type => $data)
+                        <div class="row" style="color: {{$data->isAvailable(0) ? 'green' : 'red'}}" >
+              <div class="col-md-1">
+                <input     type="checkbox" 
+                       name="order[{{$data->id}}]" 
+                      value="{{$data->id}}" 
+                      class="" 
+                    data-toggle="popover"
+                    data-action="calcTotal"                 
+                   data-content="${{ $data->price }}"  
+                     data-value="{{ $data->price }}" 
+                     data-product-id="{{ $data->product()->id }}" 
+                />
+              
+              </div>
+              <div class="col-md-4">
+              
+                {{$type}}
+               </div>
+               <div class="col-md-1">
+                X
+               </div>
+              <div class="col-md-4">
+                         
+                <input type="number" name="orderquantity[{{$data->id}}]" value="1">
+                          
+              </div>
+                        </div>
+
+                      @endforeach
+            Value:
+            <input type="text" value="0" name="ordervalue[{{$data->product()->id}}]" />
+            
+                </div>
+              </div>
+            </div>
+          </div>
+                      
+                    @endforeach
+                    @endforeach
+                    <br />
+                    <label>Order Notes</label>
+                    <textarea class="form-control" name="orderNotes"></textarea>
+
+          
+                    <br />
+                    <label>Order Total</label>
+                    <input type='number' class="form-control" name="orderTotal" value="0" />
+
+
+                    <label>Customer Notes</label>
+                    <textarea class="form-control" name="customerNotes">{{$customer->notes}}</textarea>
+
+                    <hr >
+                    
+                    <input type="hidden" name="locationID">
+                    <input type="hidden" name="lat">
+                    <input type="hidden" name="lon">
+                    @if($customer->id)
+                      <input type="hidden" name="customerID" value="{{$customer->id}}">
+                    @endif
+
+                    @csrf
+          
+          <label>Assigne to</label>
+          <select name="driverID" class="form-control" required>
+            <!-- <option value='' >Choose Driver</option> -->
+            <option value='0' >House</option>
+            @foreach(Auth::user()->drivers() as $driver)
+              <option value="{{$driver->id}}">{{$driver->name}}</option>
+            @endforeach
+          </select>
+            
+                    <input type="submit" class="form-control btn btn-info staticButton" name="" value="Save">
+                    </form>
+
+
+                      
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">Map</div>
+
+                <div class="card-body">
+
+                   
+
+                     <div id="address"></div>
+                     <div class="myMap" id="mymap"></div>
+                     <div class="myMap" id="mymap-out"></div>
+
+
+                      
+                </div>
+            </div>
+        </div>
+    
+    <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">Customers</div>
+
+                <div class="card-body">
+                  
+
+                  @foreach($Customers as $customer)
+                    <div class="row filtered ">
+                      <div class="col-md-4">
+                        {{$customer->name}}<br>
+                        <sup>{{$customer->phone}}</sup>
+                      </div>
+                      <div class="col-md-8 alignRight">
+          <a href="/neworder/{{$customer->id}}" class="">
+            {{$customer->address}}<br>
+                       <sup> {{count($customer->orders())}} Orders</sup>
+            </a>
+                      </div>
+                     
+                    </div>
+
+                  @endforeach
+
+
+                      <script type="text/javascript">
+
+
+                    
+
+                      </script>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<script type="text/javascript">
+$(function () {
+  $('[data-toggle="popover"]').popover({
+  trigger: 'hover'
+  })
+});
+
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $(".filtered").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+  
+  $('[data-action="calcTotal"]').on("click", function() {
+    id = $(this).data('product-id');
+    price = parseInt($(this).data('value'));
+    
+    //curPrice = parseInt($('[name="ordervalue['+id+']"').val());
+    orderquantity = parseInt($('[name="orderquantity['+$(this).val()+']"').val());
+    if(!isNaN(orderquantity))
+      price *= orderquantity;
+    $('[name="ordervalue['+id+']"').val(price);
+    
+  });
+});
+
+
+            $('#getorder').click(function(){
+                address = $('[name="address"]').val();
+
+                var geocoder = new google.maps.Geocoder();
+
+                geocoder.geocode({'address': address}, function(results, status) 
+                {
+                    console.log(address,results);
+
+                    lat = results[0].geometry.location.lat();
+                    lon = results[0].geometry.location.lng();
+                    addFull = results[0].formatted_address;
+                    add = results[0].address_components[0].long_name+' '+results[0].address_components[1].short_name;
+
+                    city = '';
+                    //results[0].address_components[2].long_name+', '+results[0].address_components[4].short_name;
+                    $(results[0].address_components).each(function(){
+                        // console.log(this);
+                        if(this.types.includes('locality'))
+                        {
+                            city += this.long_name;
+                        }
+                        if(this.types.includes('administrative_area_level_1'))
+                        {
+                            city += ', '+this.short_name;
+                        }
+                    });
+
+                    $('[name="lat"]').val(lat);
+                    $('[name="lon"]').val(lon);
+                    $('[name="locationID"]').val(results[0].place_id);
+                    $('[name="address"]').val(add+', '+city);
+                    $('#address').html(addFull);
+                    
+                    var mymap = new GMaps({
+                          el: '#mymap',
+                          lat: lat,
+                          lng: lon,
+                          zoom:15
+                        });
+
+                    mymap.addMarker({
+                              lat: lat,
+                              lng: lon,
+                              title: address,
+                              click: function(e) {
+                                // alert('This is '+value.name+'.');
+                              }
+                            });
+
+                    var mymap2 = new GMaps({
+                          el: '#mymap-out',
+                          lat: lat,
+                          lng: lon,
+                          zoom:10
+                        });
+
+                    mymap2.addMarker({
+                              lat: lat,
+                              lng: lon,
+                              title: address,
+                              click: function(e) {
+                                // alert('This is '+value.name+'.');
+                              }
+                            });
+
+                    // $('[name="city"]').val(city);
+                  // if (status === 'OK') {
+                  //   resultsMap.setCenter(results[0].geometry.order);
+                  //   var marker = new google.maps.Marker({
+                  //     map: resultsMap,
+                  //     position: results[0].geometry.order
+                  //   });
+                  // } else {
+                  //   alert('Geocode was not successful for the following reason: ' + status);
+                  // }
+                });
+            });
+
+            @if($customer->id)
+                $('#getorder').trigger('click');
+            @endif
+
+</script>
+                      </script>
+@endsection
